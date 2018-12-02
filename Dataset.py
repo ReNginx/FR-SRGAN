4638a39189ec94409db9a847ccc3acc56a143ec7
rymaspic <rymaspic@outlook.com>
2018-12-01 20:44:07 -0500

    updates: load the data(LR) and its label(HR)

--- Dataset.py
+++ Dataset.py
@@ -4,13 +4,6 @@ import torch.utils.data as data
 import os
 from PIL import Image
 
-data_dir = 'Data/vimeo_super_resolution_test/low_resolution/00001/'
-batch = 4 # batch size of the data every time for training
-batch_number = 100000  # number of batches, so we totally have batch_number * batch images
-frame_counter = 7 # number of frames per folder
-height = 64
-weight = 112
-
 base_transform = transforms.Compose([
     transforms.ToTensor(),
     #transforms.Normalize([0.5]*3, [0.5]*3)
@@ -23,19 +16,21 @@ def image_loader(path):
 
 class FRDataset(data.Dataset):
 
-    def __init__(self,dir):
+    def __init__(self,dir,height,weight):
         self.file_dir = dir
         self.transform = base_transform
         #self.image_loader = image_loader()
         self.frames_set = os.listdir(self.file_dir)
+        self.height = height
+        self.weight = weight
 
     def __getitem__(self, index):
 
         frames = self.frames_set[index] #0266
-        frame_tensor = torch.Tensor(size=(frame_counter, 3, height, weight))
+        frame_tensor = torch.Tensor(size=(frame_counter, 3, self.height, self.weight))
         for img in frames:
-            file_dir_frames = self.file_dir + frames
-            #file_dir_frames = '/'.os.path.join(self.file_dir,frames)
+            #file_dir_frames = self.file_dir + frames
+            file_dir_frames = os.path.join(self.file_dir,frames)
             imgs_path = os.listdir(file_dir_frames)
             imgs_path.sort()
             i = 0
@@ -43,6 +38,7 @@ class FRDataset(data.Dataset):
                 final_path = file_dir_frames + "/" + img
                 #final_path = '/'.os.listdir(file_dir_frames,img)
                 img_tensor = image_loader(final_path)
+                #print(img_tensor.size())
                 frame_tensor[i] = img_tensor
                 i = i + 1
         return frame_tensor
@@ -50,14 +46,31 @@ class FRDataset(data.Dataset):
     def __len__(self):
         return len(self.frames_set)
 
+batch = 4 # batch size of the data every time for training
+batch_number = 100000  # number of batches, so we totally have batch_number * batch images
+frame_counter = 7 # number of frames per folder
+LR_height = 64
+LR_weight = 112
+HR_height = 256
+HR_weight = 448
 
-FRData = FRDataset(data_dir)
+train_dir_LR = 'Data/LR'
+train_dir_HR = 'Data/HR'
 
-data_loader = data.DataLoader(FRData, batch_size = batch)
+FRData_LR = FRDataset(dir=train_dir_LR,weight=LR_weight,height=LR_height)
+FRData_HR = FRDataset(dir=train_dir_HR,weight=HR_weight,height=HR_height)
+
+data_loader_LR = data.DataLoader(FRData_LR, batch_size = batch, shuffle = True)
+data_loader_HR = data.DataLoader(FRData_HR, batch_size = batch, shuffle = True)
 
 #print(data_loader[0].size())
 
-for i_batch, sample_batched in enumerate(data_loader):
+for i_batch, sample_batched in enumerate(zip(data_loader_LR,data_loader_HR)):
        #print(sample_batched)
-       permuted_data = sample_batched.permute(1,0,2,3,4)
-       print(permuted_data.size())
+       #print(data_loader_HR[i_batch].size())
+       permuted_LR_data = sample_batched[0].permute(1, 0, 2, 3, 4)
+       permuted_HR_data = sample_batched[1].permute(1, 0, 2, 3, 4) #labels
+       #print(permuted_data.contiguous())
+       print("LR:",permuted_LR_data.size())
+       print("HR:",permuted_LR_data.size())
+
