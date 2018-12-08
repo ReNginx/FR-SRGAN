4c36fc6ade8941cf87013b591ec77077691d7116
HanxRen <969765303@qq.com>
2018-12-07 19:51:57 -0500

    bug fix

--- checkTrain.py
+++ checkTrain.py
@@ -15,6 +15,12 @@ from skimage import img_as_ubyte
 from skimage.util import img_as_float32
 
 
+def trunc(tensor):
+    # tensor = tensor.clone()
+    tensor[tensor < 0] = 0
+    tensor[tensor > 1] = 1
+    return tensor
+
 def test_optic_flow(frame1, frame2):
     # im1 = img_as_ubyte(frame1)
     # im2 = img_as_ubyte(frame2)
@@ -72,83 +78,113 @@ def psnr(img1, img2):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test Single Video')
-    parser.add_argument('--model', default='./models/FRVSR.XS2', type=str, help='generator model epoch name')
+    parser.add_argument('--model', default='./models/FRVSR.20', type=str, help='generator model epoch name')
     opt = parser.parse_args()
 
     UPSCALE_FACTOR = 4
     MODEL_NAME = opt.model
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = FRVSR.FRVSR(0, 0, 0).eval()
+    model = FRVSR.FRVSR(0, 0, 0)
     model.to(device)
 
     # for cpu
     # model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
-    model.load_state_dict(torch.load(MODEL_NAME, device))
-    model.eval()
+    checkpoint = torch.load(MODEL_NAME, map_location='cpu')
+    model.load_state_dict(checkpoint)
+    model.train()
 
-    train_loader, val_loader = Dataset.get_data_loaders(1, False)
-    lr_example, hr_example = next(iter(train_loader))
+    train_loader, val_loader = Dataset.get_data_loaders(1, dataset_size=4, validation_split=1, shuffle_dataset=True)
+    for lr_example, hr_example in val_loader:
+        fps = 24
+        frame_numbers = 7
+        # frame_numbers = 100
+        lr_width = lr_example.shape[4]
+        lr_height = lr_example.shape[3]
+        model.set_param(batch_size=1, width=lr_width, height=lr_height)
+        print(model.width, model.height)
+        model.init_hidden(device)
 
-    fps = 24
-    frame_numbers = 7
-    # frame_numbers = 100
-    lr_width = lr_example.shape[4]
-    lr_height = lr_example.shape[3]
-    model.set_param(batch_size=1, width=lr_width, height=lr_height)
-    model.init_hidden(device)
+        hr_video_size = (lr_width * UPSCALE_FACTOR,
+                         lr_height * UPSCALE_FACTOR)
+        lr_video_size = (lr_width, lr_height)
 
-    hr_video_size = (lr_width * UPSCALE_FACTOR,
-                     lr_height * UPSCALE_FACTOR)
-    lr_video_size = (lr_width, lr_height)
+        output_sr_name = 'out_srf_' + str(UPSCALE_FACTOR) + '_' + 'random_sample.mp4'
+        output_gt_name = 'out_srf_' + 'groundtruth' + '_' + 'random_sample.mp4'
+        output_lr_name = 'out_srf_' + 'original' + '_' + 'random_sample.mp4'
+        output_aw_name = 'out_srf_' + 'warp' + '_' + 'random_sample.mp4'
 
-    output_sr_name = 'out_srf_' + str(UPSCALE_FACTOR) + '_' + 'random_sample.mp4'
-    output_lr_name = 'out_srf_' + 'original' + '_' + 'random_sample.mp4'
-    output_aw_name = 'out_srf_' + 'warp' + '_' + 'random_sample.mp4'
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        hr_video_writer = cv2.VideoWriter(output_sr_name, fourcc, fps, hr_video_size)
+        lr_video_writer = cv2.VideoWriter(output_lr_name, fourcc, fps, lr_video_size)
+        aw_video_writer = cv2.VideoWriter(output_aw_name, fourcc, fps, hr_video_size)
+        gt_video_writer = cv2.VideoWriter(output_gt_name, fourcc, fps, hr_video_size)
+        # read frame
+        # test_optic_flow(lr_example[0][0].permute(1,2,0).numpy(), \
+        #                  lr_example[1][0].permute(1,2,0).numpy())
 
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-    hr_video_writer = cv2.VideoWriter(output_sr_name, fourcc, fps, hr_video_size)
-    lr_video_writer = cv2.VideoWriter(output_lr_name, fourcc, fps, lr_video_size)
-    aw_video_writer = cv2.VideoWriter(output_aw_name, fourcc, fps, hr_video_size)
-    # read frame
-    # test_optic_flow(lr_example[0][0].permute(1,2,0).numpy(), \
-    #                  lr_example[1][0].permute(1,2,0).numpy())
+        for image, truth in zip(lr_example, hr_example):
+            # plt.subplot(121)
+            # plt.imshow(image[0].permute(1,2,0).numpy())
+            # plt.subplot(122)
+            # plt.imshow(truth[0].permute(1,2,0).numpy())
+            # plt.show()
+            # exit(0)
+            # image.to(device)
+            # print(f'image shape is {image.shape}')
+            # if torch.cuda.is_available():
+            #     image = image.cuda()
 
-    out_psnr = 0
+            hr_out, lr_out = model(image)
+            hr_out = hr_out.clone()
+            lr_out = lr_out.clone()
+            # plt.imshow(hr_out[0].permute(1,2,0).detach().numpy())
+            # plt.imshow(truth[0].permute(1,2,0).clone().numpy())
+            # plt.show()
+            print(image.shape)
+            print(lr_out.shape)
+            l1 = torch.mean((truth - hr_out) ** 2)
+            l2 = torch.mean((image - lr_out) ** 2)
+            print(l1)
+            print(l2)
+            # print(lr_out)
+            # print(image)
+            hr_out = Dataset.inverse_transform(hr_out.clone())
+            lr_out = Dataset.inverse_transform(lr_out.clone())
+            image = Dataset.inverse_transform(image.clone())
+            truth = Dataset.inverse_transform(truth.clone())
+            hr_out = trunc(hr_out.clone())
+            lr_out = trunc(lr_out.clone())
+            aw_out = model.afterWarp.clone()
 
-    for image, ground_truth in zip(lr_example, hr_example):
-        image.to(device)
-        # print(f'image shape is {image.shape}')
-        # if torch.cuda.is_available():
-        #     image = image.cuda()
+            l1 = torch.mean((truth - hr_out) ** 2)
+            l2 = torch.mean((image - lr_out) ** 2)
+            print(l1)
+            print(l2)
 
-        hr_out, lr_out = model(image)
-        hr_out = hr_out.clone()
-        lr_out = lr_out.clone()
-        aw_out = model.afterWarp.clone()
-        print((hr_out.data.numpy()-ground_truth.data.numpy()).shape)
-        out_psnr = out_psnr + psnr(hr_out.data.numpy(),ground_truth.data.numpy()) / 3.0
-
-    #print("psnr:" + str(psnr))
-
-
-        #model.init_hidden(device)
-        def output(out, writer):
-            out = out.cpu()
-            out_img = out.data[0].numpy()
-            out_img *= 255.0
-            out_img = (np.uint8(out_img)).transpose((1, 2, 0))
-            # save sr video
-            writer.write(out_img)
-
-
-        output(hr_out, hr_video_writer)
-        output(lr_out, lr_video_writer)
-        output(aw_out, aw_video_writer)
-
-    hr_video_writer.release()
-    lr_video_writer.release()
-    aw_video_writer.release()
-    print("psnr:" + str(out_psnr))
+            plt.imshow(hr_out[0].permute(1, 2, 0).detach().numpy())
+            # plt.imshow(truth[0].permute(1,2,0).clone().numpy())
+            # plt.imshow(lr_out[0].permute(1, 2, 0).detach().numpy())
+            plt.show()
 
 
+            # model.init_hidden(device)
+            def output(out, writer):
+                out = out.clone()
+                out_img = out.data[0].numpy()
+                out_img *= 255.0
+                out_img = (np.uint8(out_img)).transpose((1, 2, 0))
+                # save sr video
+                writer.write(out_img)
+
+
+            output(hr_out, hr_video_writer)
+            output(lr_out, lr_video_writer)
+            output(aw_out, aw_video_writer)
+            output(truth, gt_video_writer)
+
+        hr_video_writer.release()
+        lr_video_writer.release()
+        aw_video_writer.release()
+        gt_video_writer.release()
+        break
